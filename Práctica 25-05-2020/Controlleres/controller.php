4931fd3c56fe95c5e1a2d296b42b2414a6bd8ab5
Obed-Rosales <44353782+Obed-Rosales@users.noreply.github.com>
2020-05-25 08:52:54 -0500

    Agregado de template Admin LTE

--- Práctica 25-05-2020/Controlleres/controller.php
+++ Práctica 25-05-2020/Controlleres/controller.php
@@ -1,3 +1,45 @@
 <?php
+    class MvcController{
 
+        public function ingresoUsuarioController(){
+            if (isset($_POST["txtUser"]) && isset($_POST["txtPassword"])) {
+                $datosController=array ("user" => $_POST["txtUser"], "password" => $_POST["txtPassword"]);
+                $respuesta = Datos::ingresoUsuarioModel($datosController, "users");
+    
+                //Validar la respuesta del modelo para ver si es un usuario correcto
+                if($respuesta["usuario"]== $_POST["txtUser"] && password_verify($_POST["passwordIngreso"], $respuesta['contrasena'])){
+                    session_start();
+                    $SESSION["validar"] = true;
+                    header("locarion:index.php?action=usuarios");
+                }else{
+                    header("location:index.php?action=fallo");
+                }
+            }
+        }
+
+
+
+
+        //VISTA DE USUARIOS
+    public function vistaUsuariosController(){
+        $respuesta = Datos::vistaUsuarioModel("users");
+        //Utilizar un foreach para iterar un array e imprimir la consulta del modelo
+        foreach($respuesta as $row => $item){
+            echo'
+                <tr>
+                <td><a href="index.php?action=editar&id='.$item["id"].'"><button>Editar</button></a></td>
+                    <td>'.$item["usuario"].'</td>
+                    <td>'.$item["password"].'</td>
+                    <td>'.$item["email"].'</td>
+                    
+                    <td><a href="index.php?action=usuarios&idBorrar='.$item["id"].'"><button>Borrar</button></a></td>
+                </tr>';
+        }
+    }
+
+    
+
+
+
+    }
 ?>
