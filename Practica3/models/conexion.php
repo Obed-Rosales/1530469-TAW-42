f90536e581c067d466d8d15be8398a6fa79d8e8a
Obed-Rosales <44353782+Obed-Rosales@users.noreply.github.com>
2020-06-12 09:02:56 -0500

    Update conexion.php

--- Practica3/models/conexion.php
+++ Practica3/models/conexion.php
@@ -1,7 +1,7 @@
 <?php 	
 	class Conexion{ //Crea la clase para realizar la conexión con la base de datos	
 		function conectar(){ //Crea la función que permite conectar con la base de datos
-			//$link = new PDO ("mysql:host=localhost;dbname=inventario","cecii", "CeciiVictoria98p"); //Crea un objeto de conexión el cual almacena el host, el nombre de la bd, el usuario y contraseña.
+			//Crea un objeto de conexión el cual almacena el host, el nombre de la bd, el usuario y contraseña.
 			$link = new PDO ("mysql:host=localhost;dbname=inventario","obed", "obed");
 			return $link; //Regresa el objeto creado para la conexión.
 		} //Termina la función conectar.
